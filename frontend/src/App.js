15bdf84caa1b0e6a0095fa930abb990b3ddbe465
Anugrah1712 <anugrahmishra15@gmail.com>
2025-11-09 20:22:52 +0530

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
 
     try {
       setLoading(true);
-      const response = await fetch("http://127.0.0.1:8000/generate", {
+      const response = await fetch("/generate", {
         method: "POST",
         body: formData,
       });
